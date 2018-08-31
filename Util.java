fdb8771d386f175319aaae04c2837994144eaada
mbusha1991 <42760753+mbusha1991@users.noreply.github.com>
2018-08-31 10:01:07 +0530

    Create Util.java

---
+++ Util.java
@@ -0,0 +1,15 @@
+package com.qa.util;
+
+public class Util {
+	
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	
+	public static long IMPLICIT_WAIT = 20;
+	
+	public static long EXPLICIT_WAIT = 20;
+	
+	
+
+
+	
+}
