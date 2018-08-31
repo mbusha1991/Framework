f8bc100fd3516de6f1c30dbe272dada562b940ec
mbusha1991 <42760753+mbusha1991@users.noreply.github.com>
2018-08-31 09:56:37 +0530

    Create WebEventListner.java

---
+++ WebEventListner.java
@@ -0,0 +1,252 @@
+package com.qa.Base;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+
+public class WebEventListner extends TestBase implements WebDriverEventListener{
+	
+	 private static final Logger log = LogManager.getLogger(WebEventListner.class);
+	 
+	public void beforeNavigateTo(String url, WebDriver driver) {
+		log.info("Before navigating to: '" + url + "'");
+	}
+
+	public void afterNavigateTo(String url, WebDriver driver) {
+		log.info("Navigated to:'" + url + "'");
+	}
+
+	public void beforeChangeValueOf(WebElement element, WebDriver driver) {
+		log.info("Value of the:" + element.toString() + " before any changes made");
+	}
+
+	public void afterChangeValueOf(WebElement element, WebDriver driver) {
+		log.info("Element value changed to: " + element.toString());
+	}
+
+	public void beforeClickOn(WebElement element, WebDriver driver) {
+		log.info("Trying to click on: " + element.toString());
+	}
+
+	public void afterClickOn(WebElement element, WebDriver driver) {
+		log.info("Clicked on: " + element.toString());
+	}
+
+	public void beforeNavigateBack(WebDriver driver) {
+		log.info("Navigating back to previous page");
+	}
+
+	public void afterNavigateBack(WebDriver driver) {
+		log.info("Navigated back to previous page");
+	}
+
+	public void beforeNavigateForward(WebDriver driver) {
+		log.info("Navigating forward to next page");
+	}
+
+	public void afterNavigateForward(WebDriver driver) {
+		log.info("Navigated forward to next page");
+	}
+
+	public void onException(Throwable error, WebDriver driver) {
+		
+		try {
+			getScreenshot();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		log.info("Exception occured: " + error);
+	}
+
+	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+		log.info("Trying to find Element By : " + by.toString());
+	}
+
+	public void afterFindBy(By by, WebElement element, WebDriver driver) {
+		log.info("Found Element By : " + by.toString());
+	}
+
+	/*
+	 * non overridden methods of WebListener class
+	 */
+	public void beforeScript(String script, WebDriver driver) {
+	}
+
+	public void afterScript(String script, WebDriver driver) {
+	}
+
+	public void beforeAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+
+     }
+	/*@Override
+	public void afterAlertAccept(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterAlertDismiss(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterClickOn(WebElement arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterFindBy(By arg0, WebElement arg1, WebDriver arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateBack(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateForward(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateRefresh(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateTo(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterScript(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeAlertAccept(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeAlertDismiss(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeClickOn(WebElement arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeFindBy(By arg0, WebElement arg1, WebDriver arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateBack(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateForward(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateRefresh(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateTo(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeScript(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onException(Throwable arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	*/
+	
+
+}
